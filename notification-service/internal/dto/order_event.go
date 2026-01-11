35968b65a7374906fecd2d857c91b51a13169cec
Muhammad Habib <mhabib34official@gmail.com>
2026-01-11 20:37:38 +0700

    refactor: refactor notification dto

--- notification-service/internal/dto/order_event.go
+++ notification-service/internal/dto/order_event.go
@@ -1,40 +1,9 @@
 package dto
 
-import (
-	"time"
-
-	"github.com/gofrs/uuid"
-)
+import "github.com/gofrs/uuid"
 
 type OrderCreatedEvent struct {
-	OrderID     uuid.UUID  `json:"OrderID"`      // PascalCase!
-	Email       string  `json:"Email"`        // PascalCase!
-	TotalAmount float64 `json:"TotalAmount"`  // PascalCase!
-}
-
-
-type CreateNotificationRequest struct {
-	OrderID uuid.UUID `json:"order_id"`
-	Type    string    `json:"type"`
-	Message string    `json:"message"`
-}
-
-type SearchNotificationRequest struct {
-	IsRead *bool  `json:"is_read"`
-	Type   string `json:"type"`
-	Limit  int    `json:"limit"`
-	Page   int    `json:"page"`
-}
-
-type MarkNotificationAsReadRequest struct {
-	IsRead bool `validate:"required" json:"is_read"`
-}
-
-type NotificationResponse struct {
-	ID        uuid.UUID `json:"id"`
-	OrderID   uuid.UUID `json:"order_id"`
-	Type      string    `json:"type"`
-	Message   string    `json:"message"`
-	IsRead    bool      `json:"is_read"`
-	CreatedAt time.Time `json:"created_at"`
+	OrderID     uuid.UUID `json:"OrderID"`     // PascalCase!
+	Email       string    `json:"Email"`       // PascalCase!
+	TotalAmount float64   `json:"TotalAmount"` // PascalCase!
 }
